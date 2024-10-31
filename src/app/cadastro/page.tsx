1a558df31a95b23a6b08b17032a494879804907a
pedroonovais <phenrique101007@gmail.com>
2024-10-31 14:32:36 -0300

    Adiciona tela de cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -13,8 +13,13 @@ import { Wrapper } from "@/components/Wrapper/Wrapper";
 export default function Login () {
     const { formData, setFormData } = useContext(FormContext)
     const router = useRouter()
+    const [nome, setNome] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [clientePorto, setClientePorto] = useState('')
+    const [email, setEmail] = useState('')
     const [login, setLogin] = useState('')
     const [senha, setSenha] = useState('')
+    const [confirmaSenha, setConfirmaSenha] = useState('')
     
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
@@ -22,32 +27,60 @@ export default function Login () {
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.id === "login"){
-            setLogin(event.target.value)
-        }else{
-            setSenha(event.target.value)
+        switch (event.target.id) {
+            case "nome":
+                setNome(event.target.value)
+                break;
+            case "cpf":
+                setCpf(event.target.value)
+                break;
+            case "email":
+                setEmail(event.target.value)
+                break;
+            case "senha":
+                setSenha(event.target.value)
+                break;
+            case "confirma-senha":
+                setConfirmaSenha(event.target.value)
+                break
+            case "cliente-porto":
+                setClientePorto(event.target.value)
+                break;
         }
     }
     
     return (
         <Hero strImg="/bannerAtendimento.jpg" height="100vh">
             <FormLayout minWidth="320px">
-                <h2>Login</h2>
+                <h2>Cadastro de usuário</h2>
+                <h4>Nome Completo *</h4>
                 <Input 
                     type="text"
-                    id="login"
-                    name="login"
-                    placeholder="Informe seu login"
+                    id="nome"
+                    name="nome"
+                    placeholder="Ex: João da Silva Martins"
                     value={login}
                     maxLength={16}
                     onChange={handleChange}
                 ></Input>
+                
+                <h4>CPF *</h4>
                 <Input 
-                    type="password"
+                    type="text"
                     id="senha"
                     name="senha"
-                    placeholder="Digite sua senha"
+                    placeholder="Ex: 456.756.345-77"
                     value={senha}
+                    maxLength={16}
+                    onChange={handleChange}
+                ></Input>
+                <h4>Email *</h4>
+                <Input 
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Ex: joao.silva@gmail.com"
+                    value={email}
                     maxLength={16}
                     onChange={handleChange}
                 ></Input>
@@ -55,7 +88,7 @@ export default function Login () {
                     <Button onClick={handleClick} >Entrar</Button>
                 </FlexRow>
                 <Wrapper>
-                    <Link href="/cadastro/">Criar conta</Link>
+                    <Link href="/login/">Já tem cadastro? Login</Link>
                 </Wrapper>
             </FormLayout>
         </Hero>
